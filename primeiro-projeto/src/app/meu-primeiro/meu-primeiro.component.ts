953fb00a916c102d0e2de8773aec7709524ea7a9
Cledson Pacheco <cledson.caetano@gmail.com>
2017-04-11 22:25:05 -0300

    Alterar texto do hello workd

--- primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'meu-primeiro-component',
     template: `
-        <p> Meu Primeiro Component com Angular 2!" </p>
+        <p> Meu Primeiro Component com Angular 2!!!" </p>
     `
 })
 export class MeuPrimeiroComponent { }
